4b6d95de430e54dda0bcdc034ea783c7f8968f90
Kiran Katariya <kirankatariya8778@gmail.com>
2016-06-23 14:09:38 +0530

    Changed web-host route

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 //Exporting database configuration ===============================================
 module.exports = {
-		'webhost': 'kiran-shorturl.herokuapp.com/',
+		'webhost': 'https://kiran-shorturl.herokuapp.com/',
 		'url' : 'mongodb://kiruakademon:kiruakademon@ds015194.mlab.com:15194/online-judge',
 };
